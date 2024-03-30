325d3d8ff55cfa95e824482b1809be01efd3e831
NIKOLAY PETROV NIKOLOV <Nikolay.Petrov@alu.uclm.es>
2024-03-30 04:57:23 +0100

    - Se ha comentado la clase encargada de mostrar las preguntas una vez se inicia el cuestionario.

--- renderer.php
+++ renderer.php
@@ -2,30 +2,50 @@
 
 defined('MOODLE_INTERNAL') || die();
 
+/**
+ * Renderer class for the sqlquestion question type.
+ * 
+ * This class is responsible for producing the actual HTML output
+ * for questions of type sqlquestion, both during attempts and when reviewing.
+ */
 class qtype_sqlquestion_renderer extends qtype_renderer
 {
-
+    /**
+     * Generates the question display and input controls.
+     * 
+     * This method is called by Moodle to generate the HTML needed to display
+     * the question to the user, including the question text, any related concepts,
+     * the data (usually SQL code), instructions for solving the question, and the solution.
+     *
+     * @param question_attempt $qa The question attempt object.
+     * @param question_display_options $options Display options that affect the output.
+     * @return string The HTML to output.
+     */
     public function formulation_and_controls(question_attempt $qa, question_display_options $options)
     {
-
         $question = $qa->get_question();
         $output = '';
-        // Para la presentación del enunciado en la visualización de las preguntas del ejercicio.
-        $output .= html_writer::tag('h3', get_string('statement_tittle', 'qtype_sqlquestion'), array('class' => 'sqlquestion_heading'));
+
+        // Displays the question statement.
+        $output .= html_writer::tag('h3', get_string('statement_title', 'qtype_sqlquestion'), array('class' => 'sqlquestion_heading'));
         $output .= html_writer::tag('div', $question->format_questiontext($qa), array('class' => 'qtext'));
+
+        // If there are related concepts, displays them.
         if (!empty($question->relatedconcepts)) {
             $output .= html_writer::tag('h3', get_string('statement_concepts', 'qtype_sqlquestion'), array('class' => 'sqlquestion_heading'));
             $output .= html_writer::tag('div', format_text($question->relatedconcepts), array('class' => 'sqlquestion_relatedconcepts'));
         }
 
+        // Displays the data section, typically containing SQL code.
         $output .= html_writer::tag('h3', get_string('statement_data', 'qtype_sqlquestion'), array('class' => 'sqlquestion_heading'));
-        // El uso de pre es para conservar la forma del codigo.
-        // El uso de s() en lugar de formmat asegura que cualquier carácter especial en el script (como < o >) se escape correctamente y no se interprete como HTML.
+        // Use of <pre> to preserve formatting of the code.
         $output .= html_writer::tag('pre', s($question->data), array('class' => 'sqlquestion_data'));
 
+        // Displays the instructions for solving the question.
         $output .= html_writer::tag('h3', get_string('statement_instructions', 'qtype_sqlquestion'), array('class' => 'sqlquestion_heading'));
         $output .= html_writer::tag('div', format_text($question->instructions), array('class' => 'sqlquestion_instructions'));
 
+        // Displays the solution.
         $output .= html_writer::tag('h3', get_string('statement_solution', 'qtype_sqlquestion'), array('class' => 'sqlquestion_heading'));
         $output .= html_writer::tag('div', format_text($question->solution), array('class' => 'sqlquestion_solution'));
 
