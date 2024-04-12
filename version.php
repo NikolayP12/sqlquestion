2a589946e033cececa5b362b9ffc0932cc065013
NIKOLAY PETROV NIKOLOV <Nikolay.Petrov@alu.uclm.es>
2024-04-12 20:01:34 +0200

    - Se ha la version del plugin

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'qtype_sqlquestion';
-$plugin->release = '0.1.0';
+$plugin->release = '1.2.0';
 $plugin->version = 2024032205;
 $plugin->requires = 2022112800;
 $plugin->maturity = MATURITY_ALPHA;
