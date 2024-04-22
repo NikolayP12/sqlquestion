795e0dd29c440d017df1822376c12ce1b7671da8
NIKOLAY PETROV NIKOLOV <Nikolay.Petrov@alu.uclm.es>
2024-04-22 23:20:25 +0200

    - Se han actualizado las versiones tanto de del install.xml, como la version del plugin y el estado de madurez del plugin

--- version.php
+++ version.php
@@ -26,6 +26,6 @@ defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'qtype_sqlquestion';
 $plugin->release = '1.2.0';
-$plugin->version = 2024032205;
+$plugin->version = 2024032206;
 $plugin->requires = 2022112800;
-$plugin->maturity = MATURITY_ALPHA;
+$plugin->maturity = MATURITY_STABLE;
